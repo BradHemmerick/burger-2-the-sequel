25092dfefe5be667f08166ba8449c22ac6812bc7
BradHemmerick <Brad.hemmerick13@gmail.com>
2018-01-09 13:30:16 -0600

    initial commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/burgers", function(req, res) {
+        db.Todo.findAll({}).then(function(dbBurger) {
+          res.json(dbBurger);
+        });
+      });
+
+      app.post("/api/burgers", function(req, res) {
+        db.Burger.create({
+          name: req.body.name
+        }).then(function(dbBurger) {
+          res.json(dbBurger);
+        });
+      });
+
+      app.put("/api/burgers", function(req, res) {
+        db.Burger.update({
+          name: req.body.name,
+          devoured: req.body.devoured
+        }, {
+          where: {
+            id: req.body.id
+          }
+        })
+        .then(function(dbBurger) {
+          res.json(dbBurger);
+        });
+      });
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Set up express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// require models
+const db = require("./models");
+
+// Set up body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// serve static pages
+app.use(express.static("public"));
+
+//require my route
+require("./routes/api-routes.js")(app);
+
+// Sync sequelize and start express
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+  });
+});
